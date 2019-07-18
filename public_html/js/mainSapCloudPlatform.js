26cb8811532e4c3339071bba58097d590f008839
migty44 <migty@mail.ru>
2019-07-18 20:04:31 +0300

    поправили треугольники на svg подправить адаптацию

--- public_html/js/mainSapCloudPlatform.js
+++ public_html/js/mainSapCloudPlatform.js
@@ -1,18 +1,101 @@
-$('#hamburger').on('click', function(event) {
-    $('#mobileMenu').toggle().animate({'right': '0'}, 700);
-    $('#mobileMenu').toggleClass('menuScrolled');
-    $(this).toggleClass('mobileMenu_activated');
-    $('body').toggleClass('bodyNoScrolled');
-});
-
-$('#mobileMenu a.has_child').on('click', function(e) {
-    e.preventDefault();
-    $(this).next( ".topNav_2levelList_m").slideToggle( "slow", function() {
-        // Animation complete.
-    });
-});
-
 $(document).ready(function () {
+
+    // строка блок функционал
+
+    var secrviceDescrToggle = 0,
+        lastButtonPressed = false;
+
+
+    $('.services__item').each(function(index, value) {
+
+        //console.log(index + ' ' + value);
+
+        var thatTransport = $(this).siblings('.services__desc');
+
+        $(this).on('click', function () {
+            var thatDescr = $(this).find('.services__itemContentDescr').html();
+
+            if (!secrviceDescrToggle) {
+                thatTransport.html(thatDescr).slideDown();
+                secrviceDescrToggle = 1;
+            } else {
+
+                if (lastButtonPressed === index){
+                    console.log('уже нажимали!');
+                    thatTransport.html('').slideUp();
+                    secrviceDescrToggle = 0;
+                } else {
+                    thatTransport.html(thatDescr);
+                }
+
+            }
+
+
+            lastButtonPressed = index;
+
+        });
+    });
+
+
+
+    // $('.services__item').on('click', function (event) {
+    //
+    //     var thatDescr = $(this).find('.services__itemContentDescr').html();
+    //
+    //     if (!secrviceDescrToggle ){
+    //         $('.services__desc').html(thatDescr).slideDown();
+    //
+    //         thatItemObj = $(this);
+    //         secrviceDescrToggle = 1;
+    //
+    //         console.log (thatItemObj);
+    //
+    //         console.log ($(this));
+    //
+    //
+    //
+    //     } else if (secrviceDescrToggle && (thatItemObj === $(this))){
+    //
+    //         // console.log (typeof $(this));
+    //         // console.log (typeof thatItemObj);
+    //         //
+    //
+    //         console.log (thatItemObj);
+    //
+    //         console.log ($(this));
+    //
+    //
+    //         $('.services__desc').html(thatDescr);
+    //
+    //         thatItemObj = $(this);
+    //
+    //     } else {
+    //         $('.services__desc').html(thatDescr).slideUp();
+    //
+    //         secrviceDescrToggle = 0;
+    //
+    //         console.log ('11');
+    //     }
+    //
+    // });
+
+
+
+    $('#hamburger').on('click', function(event) {
+        $('#mobileMenu').toggle().animate({'right': '0'}, 700);
+        $('#mobileMenu').toggleClass('menuScrolled');
+        $(this).toggleClass('mobileMenu_activated');
+        $('body').toggleClass('bodyNoScrolled');
+    });
+
+    $('#mobileMenu a.has_child').on('click', function(e) {
+        e.preventDefault();
+        $(this).next( ".topNav_2levelList_m").slideToggle( "slow", function() {
+            // Animation complete.
+        });
+    });
+
+
 
     $("#slider__container").owlCarousel(
         {
